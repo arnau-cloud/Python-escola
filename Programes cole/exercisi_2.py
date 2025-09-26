9ce557bb08d32cc837c8ded91c4dee60fd71a3c2
Arnau <arnau.costa@insllagostera.cat>
2025-09-26 09:55:58 +0200

    new file:   exercisi_2.py     Segon exercisi

---
+++ Programes cole/exercisi_2.py
@@ -0,0 +1,9 @@
+txt = "Era una vegada un llenguatge anomenat Python"
+num = 7.55
+
+print(len(txt))
+print(f"La longitud de '{txt}' és: {len(txt)}")
+print(f"El minim de 30, 24, 52 i 26 és: {min(30,24,52,36)}")
+print(f"El tipus de dada de 'num' és: {type(num)}")
+print(f"La suma de 4,6,12 és: {sum((4,6,12))}")
+print(f"El nombre {num} se redondea a : {round(num)}")
