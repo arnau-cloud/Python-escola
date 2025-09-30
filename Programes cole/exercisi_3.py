1049d496934629225f10f952fb9138dea0215590
Arnau <arnau.costa@insllagostera.cat>
2025-09-30 10:39:09 +0200

    Exercisi num 3

---
+++ Programes cole/exercisi_3.py
@@ -0,0 +1,18 @@
+cadena = "Hola, ¿cómo estás hoy?"
+caracter = "o"
+print(cadena.count(caracter))
+
+subcadena = "estás"
+print(cadena.find(subcadena))
+
+cadena1 = "Hola"
+print(cadena1.isalpha())
+print(cadena.isalpha())
+
+cadena1 = "1234"
+print(cadena1.isnumeric())
+print(cadena.isnumeric())
+
+cadena1 = "Hola123"
+print(cadena1.isalnum())
+print(cadena.isalnum())
