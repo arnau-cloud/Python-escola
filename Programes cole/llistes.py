cc902dd1ce8d0b6df199f8d372ce4793dad4cd49
Arnau <arnau.costa@insllagostera.cat>
2025-10-08 10:49:19 +0200

    afegit coses a llistes.py

--- Programes cole/llistes.py
+++ Programes cole/llistes.py
@@ -9,3 +9,26 @@ print(llista)
 llista.insert(1, "ferrari")
 
 print(llista)
+
+print(llista.index("audi"))
+
+print("opel" in llista)
+
+llista.remove("opel")
+
+llista.append("kia")
+
+print(llista)
+
+llista.pop()
+
+print(llista)
+
+llista2 = list(range(1,11))
+
+print(llista2)
+
+cadena = "1, 2, 3"
+llista3 = cadena.split(", ")
+
+print(llista3)
