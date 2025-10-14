b57569efc52de9f23fb0acfedabee3a6941fd42f
Arnau <arnau.costa@insllagostera.cat>
2025-10-14 10:32:54 +0200

    afegit més programes

---
+++ Programes cole/tupla.py
@@ -0,0 +1,12 @@
+tupla1 = (1, 2, 3, 4, 5)
+print(tupla1)
+
+una_lista = ["uno", "dos", "tres", "cuatro"]
+tupla2 = tuple(una_lista)
+print(tupla2)
+
+tupla3 = 1, 2, 3, 4, 5
+print(tupla3)
+
+tupla4 = tupla1[1:3]
+print(tupla4)
